a1bd965b731cd525665eda1b4ea228081b508d63
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-08-21 15:15:12 +0200

    Make 'method' for Fizz Buzz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,18 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) System.out.print("Fizz");
+            if (i % 5 == 0) System.out.print("Buzz");
+            if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
+            System.out.println();
         }
-        //ffrrffwe
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+    /*Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number
+    and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.*/
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
     }
 }
