0cfda3bc9c7a46fe847e0816eb329975858fc105
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-08-22 10:01:28 +0200

    changed FizzBuzz counter from 100 to 200

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 200; i++) {
 
             if (i % 3 == 0) System.out.print("Fizz");
             if (i % 5 == 0) System.out.print("Buzz");
