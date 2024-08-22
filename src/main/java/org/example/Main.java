1485a5350ea66783ac8a662abac29b0f23a7d11f
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-08-22 10:02:11 +0200

    Change the fizzbuzz to buzzfizz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,14 @@ public class Main {
 
         for (int i = 1; i <= 200; i++) {
 
-            if (i % 3 == 0) System.out.print("Fizz");
-            if (i % 5 == 0) System.out.print("Buzz");
+            if (i % 3 == 0 && i % 5 != 0) System.out.print("Fizz");
+            if (i % 5 == 0 && i % 3 != 0) System.out.print("Buzz");
             if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
+            /*Commit 2: Change the main method to print BuzzFizz instead of FizzBuzz (remember to commit often and push to GitHub after each commit) and solve any merge conflicts that may occur.*/
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("BuzzFizz");
+            }
             System.out.println();
-
         }
     }
 
