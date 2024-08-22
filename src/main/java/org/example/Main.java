b3b0dcb3cfda51a1f520d3ebc812ce60903184b5
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-08-22 10:02:41 +0200

    delete task text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,6 @@ public class Main {
             if (i % 3 == 0 && i % 5 != 0) System.out.print("Fizz");
             if (i % 5 == 0 && i % 3 != 0) System.out.print("Buzz");
             if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
-            /*Commit 2: Change the main method to print BuzzFizz instead of FizzBuzz (remember to commit often and push to GitHub after each commit) and solve any merge conflicts that may occur.*/
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("BuzzFizz");
             }
